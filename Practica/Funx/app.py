a8ecb9e4c278ad72f0dac8450388461d19f4e164
Mark Smithson <marksmithsonrivas@gmail.com>
2022-12-14 10:36:19 +0100

    Frontend checkpoint :)

--- Practica/Funx/app.py
+++ Practica/Funx/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html", title="Jinja and Flask")
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
